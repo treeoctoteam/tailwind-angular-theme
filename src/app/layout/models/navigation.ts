3884789feec205cd8011dad5556af8ff66212e42
Paolo Milani <paolo.milani@esignworld.com>
2021-03-03 17:13:11 +0100

    start refactoring models

--- src/app/layout/models/navigation.ts
+++ src/app/layout/models/navigation.ts
@@ -1,25 +1,25 @@
-export interface Navigation extends NavigationItem {
-  children?: NavigationItem[];
-}
-
-export interface NavigationItem {
+interface NavigationBase {
   id: string;
+  translate: string;
   type: 'item' | 'group' | 'collapsable';
-  translate?: string;
-  icon?: string;
-  hidden?: boolean;
-  url?: string;
-  classes?: string;
-  exactMatch?: boolean;
-  externalUrl?: boolean;
-  openInNewTab?: boolean;
-  function?: any;
-  badge?: {
-    title?: string;
-    translate?: string;
-    bg?: string;
-    fg?: string;
-  };
-  children?: NavigationItem[];
+  icon: string;
+  url: string;
+  hidden: boolean;
 }
 
+export interface Navigation extends NavigationBase {
+  classes: string;
+  exactMatch: boolean;
+  externalUrl: boolean;
+  openInNewTab: boolean;
+  function: any;
+  badge: {
+    title: string;
+    translate: string;
+    bg: string;
+    fg: string;
+  };
+  children: NavigationItem[];
+}
+
+export interface NavigationItem extends NavigationBase {}
