112b19d9b9be73eb0428fab50eb3f7ab6021c4ad
Giovd8 <giovanni_vidotto@hotmail.it>
2021-04-29 15:16:42 +0200

    edit user fix

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -14,6 +14,7 @@ export interface User {
   email: string;
   role: UserRoles;
   username: string;
+  isLocked: boolean;
 }
 
 interface AuthRes {
--- src/app/modules/dashboard/pages/overview-user/overview-user.component.ts
+++ src/app/modules/dashboard/pages/overview-user/overview-user.component.ts
@@ -1,25 +1,47 @@
+import { User } from './../../../../core/services/auth.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { Subject } from 'rxjs';
 import { Response } from './../../../../shared/models/response.model';
 import { DashboardConfigService } from 'src/app/modules/dashboard/services/dashboard-config.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
+interface IUserManagement {
+  user: User;
+  options: {
+    edit: boolean,
+    editUsername: boolean,
+    editEmail: boolean
+  }
+}
+
 
 @Component({
   selector: 'octo-overview-user',
   templateUrl: './overview-user.component.html',
   styleUrls: ['./overview-user.component.scss']
 })
+
 export class OverviewUserComponent implements OnInit, OnDestroy {
 
   public usersList = [];
+  public isAdmin = false;
+  public isEditingMode = false;
+
+  public userManagement: IUserManagement[] = [];
+
   $unsubscribe = new Subject<void>();
 
-  constructor(private dashboardConfigService: DashboardConfigService) { }
+  constructor(private dashboardConfigService: DashboardConfigService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.dashboardConfigService.getUsers();
     this.dashboardConfigService.$usersList.pipe().subscribe((users: any[]) => {
       this.usersList = users;
+      users.forEach((user: User) => {
+        this.userManagement.push({ user, options: { edit: false, editEmail: false, editUsername: false } });
+      })
     });
+    this.isAdmin = this.authService.user.role === 'admin' ? true : false;
   }
 
   ngOnDestroy() {
@@ -33,8 +55,22 @@ export class OverviewUserComponent implements OnInit, OnDestroy {
     })
   }
 
-  editUser(email: string) {
-    alert(email)
+  activateEdit(um: IUserManagement) {
+    um.options.edit = true;
+    if (this.isAdmin) {
+      um.options.editUsername = true;
+      um.options.editEmail = true;
+    }
+    else {
+      um.options.editUsername = true;
+    }
+  }
+
+  editUser(um: IUserManagement) {
+    console.log(um.user, "USER");
+    um.options.edit = false;
+    um.options.editEmail = false;
+    um.options.editUsername = false;
   }
 
   onSelectChange(event: any, email: string) {
